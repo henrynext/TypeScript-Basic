382145cd878f72591351703ca67f605e594f59b4
2ono <clt3555@gmail.com>
2023-07-15 15:01:54 +0900

    destructurering obj & arrays

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
-const userName = "Henry";
-let age = 30;
+// const userName = "Henry";
+// let age = 30;
 
-age = 29;
+// age = 29;
 
 // function add(a: number, b: number) {
 //   let result = a + b;
@@ -14,20 +14,20 @@ age = 29;
 
 // console.log(isOld);
 
-const add = (a: number, b: number = 1) => a + b;
+// const add = (a: number, b: number = 1) => a + b;
 
-// console.log(add(2, 5));
+// // console.log(add(2, 5));
 
-const printOutput: (a: number | string) => void = (output) =>
-  console.log(output);
+// const printOutput: (a: number | string) => void = (output) =>
+//   console.log(output);
 
-const button = document.querySelector("button");
+// const button = document.querySelector("button");
 
-if (button) {
-  button.addEventListener("click", event => console.log(event));
-}
+// if (button) {
+//   button.addEventListener("click", event => console.log(event));
+// }
 
-printOutput(add(3));
+// printOutput(add(3));
 
 const hobbies = ['Sports', 'Cooking'];
 console.log(hobbies);
@@ -38,8 +38,8 @@ activeHobbies.push("Eating",...hobbies);
 console.log(activeHobbies);
 
 const person = {
-    name: 'Henry',
-    age: 30
+    firstName: 'Henry',
+    firstAge: 30
 };
 const copiedObj = person;
 console.log(copiedObj);
@@ -48,4 +48,22 @@ console.log(copiedObj);
 
 const copiedPerson = { ...person };
 console.log(copiedPerson);
+
+const add = (...numbers: number[]) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue;
+    }, 0);
+}
+
+const addedNumbers = add(5, 10, 2, 3.2 , 2.1);
+console.log(addedNumbers);
+
+// const hobby1 = hobbies[0];
+// const hobby2 = hobbies[1];
+
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+console.log(hobbies, hobby1, hobby2);
+
+const {firstName: userNames, firstAge} = person;
+console.log(userNames, firstAge,person);
 
