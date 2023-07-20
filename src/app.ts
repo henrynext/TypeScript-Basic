b6074fe15ca994b73c9938cc07ff005525fedb1e
2ono <clt3555@gmail.com>
2023-07-21 08:53:10 +0900

    decorators

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,30 @@
 function Logger(logString: string) {
+  console.log("Logger Factory");
+
   return function (constructor: Function) {
     console.log(logString);
     console.log(constructor);
   };
 }
 
-@Logger("Logging -Person")
+function WithTemplate(template: string, hookId: string) {
+  return function (constructor: any) {
+    console.log("Template Factory");
+
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector("h1")!.textContent = p.name;
+    }
+  };
+}
+
+// @Logger("Logging -Person")
+@Logger("Logging")
+@WithTemplate("<h1>My Person Object</h1>", "app")
 class Person {
-  name = "Henry";
+  name = "Alice";
 
   constructor() {
     console.log("Creating person object!");
@@ -17,3 +34,61 @@ class Person {
 const person = new Person();
 
 console.log(person);
+
+function Log(target: any, propertyName: string | symbol) {
+    console.log('Property decorator!');
+    console.log(target, propertyName);
+    
+    
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+    console.log('Accesor decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+    
+    
+    
+    
+}
+
+function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+    console.log('Method decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+    
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+    console.log('Parameter decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Invalid price - should be positive!");
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * (1 + tax);
+  }
+}
