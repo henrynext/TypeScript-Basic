32d4e1ddfb13788e59908502f32fa7e6d9b0f28b
2ono <clt3555@gmail.com>
2023-07-28 19:48:54 +0900

    validation with decorators

--- src/app.ts
+++ src/app.ts
@@ -8,15 +8,22 @@ function Logger(logString: string) {
 }
 
 function WithTemplate(template: string, hookId: string) {
-  return function (constructor: any) {
-    console.log("Template Factory");
+  console.log("Template Factory");
+  return function <T extends { new (...args: any[]): { name: string } }>(
+    originalConstructor: T
+  ) {
+    return class extends originalConstructor {
+      constructor(..._: any[]) {
+        super();
+        console.log("Rendering template");
 
-    const hookEl = document.getElementById(hookId);
-    const p = new constructor();
-    if (hookEl) {
-      hookEl.innerHTML = template;
-      hookEl.querySelector("h1")!.textContent = p.name;
-    }
+        const hookEl = document.getElementById(hookId);
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector("h1")!.textContent = this.name;
+        }
+      }
+    };
   };
 }
 
@@ -36,36 +43,33 @@ const person = new Person();
 console.log(person);
 
 function Log(target: any, propertyName: string | symbol) {
-    console.log('Property decorator!');
-    console.log(target, propertyName);
-    
-    
+  console.log("Property decorator!");
+  console.log(target, propertyName);
 }
 
 function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
-    console.log('Accesor decorator!');
-    console.log(target);
-    console.log(name);
-    console.log(descriptor);
-    
-    
-    
-    
+  console.log("Accesor decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
 }
 
-function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
-    console.log('Method decorator!');
-    console.log(target);
-    console.log(name);
-    console.log(descriptor);
-    
+function Log3(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log("Method decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
 }
 
 function Log4(target: any, name: string | Symbol, position: number) {
-    console.log('Parameter decorator!');
-    console.log(target);
-    console.log(name);
-    console.log(position);
+  console.log("Parameter decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(position);
 }
 
 class Product {
@@ -92,3 +96,81 @@ class Product {
     return this._price * (1 + tax);
   }
 }
+
+const p1 = new Product("Book", 19);
+const p2 = new Product("Book 2", 29);
+
+// function Autobind(_: any, _2: string, descriptor: PropertyDecorator) {
+//   const originalMethod = descriptor.value;
+//   const adjDescriptor: PropertyDecorator = {
+//     configurable: true,
+//     enumerable: false,
+//     get() {
+//       const boundFn = originalMethod.bind(this);
+//       return boundFn;
+//     }
+//   };
+//   return adjDescriptor;
+// }
+
+class Printer {
+  message = "This works!";
+
+  // @Autobind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+
+const p = new Printer();
+p.showMessage();
+const button = document.querySelector("button")!;
+button.addEventListener("click", p.showMessage);
+
+// decorator
+
+function Required() {
+
+}
+
+function PositiveNumber() {
+
+}
+
+function validate(obj: object) {
+
+}
+
+class Course {
+  @Required
+  title: string;
+  @PositiveNumber
+  price: number;
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this.price = p;
+  }
+}
+
+const courseForm = document.querySelector('form')!;
+courseForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const titleEl = document.getElementById('title') as HTMLInputElement;
+  const pirceEl = document.getElementById('price') as HTMLInputElement;
+  
+  const title = titleEl.value;
+  const price = +pirceEl.value;
+
+  
+
+  const createdCourse = new Course(title, price);
+  
+  if (!validate(createdCourse)) {
+    throw new Error('Invalid input, please try again')
+    return;
+  }
+  console.log(createdCourse);
+  
+
+})
