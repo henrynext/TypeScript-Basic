d461247c1508812087fedf3d3ffbd8823d12e166
2ono <clt3555@gmail.com>
2023-08-25 04:13:10 +0900

    [add] class validator

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,22 @@
-import _ from "lodash";
+import "reflect-metadata";
+import "es6-shim";
 
-console.log(_.shuffle([1,2,3]));
+import { Product } from "./product.model";
 
+// const products = [
+//   { title: "A carpet", price: 31.99 },
+//   { title: "A car", price: 50000.99 },
+//   { title: "notebook", price: 233.99 },
+// ];
+
+const newProd = new Product("", -5.55523);
+validate(newProd).then((errors) => {
+  if (errors.length > 0) {
+    console.log("VALIDATION ERRORS!");
+    console.log(errors);
+  } else {
+    console.log(newProd.getInformation());
+  }
+});
+
+// const p1 = new Product("A book", 20.99);
---
+++ src/product.model.ts
@@ -0,0 +1,23 @@
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+} from "class-transformer";
+import { validate} from 'class-validator'
+
+export class Product {
+  @IsNotEmpty()
+  title: string;
+  @IsNumber()
+  @IsPositive()
+  price: number;
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this.price = p;
+  }
+
+  getInformation() {
+    return [this.title, `$${this.price}`];
+  }
+}
