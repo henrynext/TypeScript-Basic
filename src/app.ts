ea225a616e275a1d7f6233f8069f6822db1b30a7
2ono <clt3555@gmail.com>
2023-08-25 03:38:47 +0900

    [add] lodash types

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,4 @@
-/// <reference path="model/drag-drop-interfaces.ts" />
-/// <reference path="model/project-model.ts" />
-/// <reference path="state/project-state.ts" />
-/// <reference path="util/project-validation.ts" />
-/// <reference path="project/project-connect.ts" />
-/// <reference path="model/project-item.ts" />
-/// <reference path="project/project-class.ts" />
-/// <reference path="project/project-input.ts" />
+import _ from "lodash";
 
-namespace App {}
+console.log(_.shuffle([1,2,3]));
+
