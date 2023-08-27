882ee95d8f4bd86a71612ed238a519e9cf047de6
2ono <clt3555@gmail.com>
2023-08-27 19:44:01 +0900

    [add] TOdo List

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { Todo } from "./todo.model";
 
 import TodoList from "./components/TodoList";
 
+import NewTodo from "./components/NewTodo";
+
+import { Route } from "react-router-dom";
 
 const App: React.FC = () => {
-  const todos = [{ id: "t1", text: "Finish the course" }];
-  return <div className="App">
-    {/* A component that adds todos */}
-    <TodoList items={todos} />
-  </div>;
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const todoAddHandler = (text: string) => {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
+  };
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
+  return (
+    <div className="App">
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
+    </div>
+  );
 };
 
 export default App;
---
+++ my-app/src/components/NewTodo.tsx
@@ -0,0 +1,32 @@
+
+import React, { useRef, useEffect } from "react";
+
+import "./NewTodo.css";
+
+type NewTodoProps = {
+    onAddTodo: (todoTest: string) => void;
+}
+
+const NewTodo: React.FC<NewTodoProps> = (props) => {
+    
+  const textInputRef = useRef<HTMLInputElement>(null);
+
+  const todoSubmitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const enteredText = textInputRef.current!.value;
+    props.onAddTodo(enteredText);
+  };
+
+  return (
+    <form onSubmit={todoSubmitHandler}>
+      <div className="form-control">
+        <label htmlFor="todo-text">Todo Text</label>
+        <input type="text" id="todo-text" ref={textInputRef}></input>
+      </div>
+      <button type="submit">ADD TODO</button>
+    </form>
+  );
+};
+
+export default NewTodo;
